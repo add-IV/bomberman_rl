e7af77d7e350ea558d42b3dfbdbaafae30be7385
add-IV <47333498+add-IV@users.noreply.github.com>
2024-09-23 12:41:36 +0200

    formatting

--- agent_code/little_guy/mcts.py
+++ agent_code/little_guy/mcts.py
@@ -8,7 +8,9 @@ ACTIONS = ["UP", "DOWN", "LEFT", "RIGHT", "WAIT", "BOMB"]
 
 
 class TreeNode:
-    def __init__(self, parent, action, game_state, prior=0, reward=0, collect_coin=False):
+    def __init__(
+        self, parent, action, game_state, prior=0, reward=0, collect_coin=False
+    ):
         self.parent = parent
         self.action = action
         self.state = game_state
@@ -31,7 +33,7 @@ class MCTS:
         time_limit=0.1,
         exploration_constant=1 / np.sqrt(2),
         temperature=5,
-        logger = None
+        logger=None,
     ):
         self.policy_net = policy_net
         self.time_limit = time_limit
@@ -43,7 +45,6 @@ class MCTS:
 
     def search(self, game_state_dict):
         game_state = GameState(**game_state_dict)
-        print(game_state.self)
         root = TreeNode(None, None, game_state)
         start_time = time.time()
         i = 0
@@ -74,15 +75,15 @@ class MCTS:
 
     def ucb(self, parent, action):
         child = parent.children[action]
-        return (
-            child.total_value / (child.visits if child.visits != 0 else 0.001)
-            + self.exploration_constant * np.sqrt(np.log(parent.visits) / child.visits)
+        return child.total_value / (
+            child.visits if child.visits != 0 else 0.001
+        ) + self.exploration_constant * np.sqrt(
+            np.log(parent.visits) / (child.visits if child.visits != 0 else 0.001)
         )
 
     def use_policy_net(self, game_state):
         value, policy = self.policy_net(state_from_game_state(game_state))
         return value, policy
-
 
     def get_value(self, game_state):
         # use the policy network to get the value of the game state
@@ -95,14 +96,21 @@ class MCTS:
         value, policy = self.use_policy_net(game_state)
         for action in ACTIONS:
             if self.state_explorer.is_action_valid(game_state, action):
-                new_state, reward, collect_coin = self.state_explorer.step(game_state, action)
+                new_state, reward, collect_coin = self.state_explorer.step(
+                    game_state, action
+                )
                 if self.state_explorer.is_terminal(new_state):
                     if self.state_explorer.is_dead(new_state):
                         reward = -1
                     else:
-                        reward=current_reward + reward
+                        reward = current_reward + reward
                 node.children[action] = TreeNode(
-                    node, action, new_state, prior=policy[0][ACTIONS.index(action)], reward=reward, collect_coin=collect_coin
+                    node,
+                    action,
+                    new_state,
+                    prior=policy[0][ACTIONS.index(action)],
+                    reward=reward,
+                    collect_coin=collect_coin,
                 )
         if not node.children:
             return -1
@@ -118,12 +126,11 @@ class MCTS:
             node = node.parent
 
     def log_tree(self, node):
-        
-        def get_node_string(node, depth = 0):
+
+        def get_node_string(node, depth=0):
             node_string = f"{'  ' * depth}{node}"
             for child in node.children.values():
                 node_string += f"\n{get_node_string(child, depth + 1)}"
             return node_string
-        
+
         self.logger.info(get_node_string(node))
-            
